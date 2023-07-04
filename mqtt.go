d488e5ce671172b91cfdbf64c0a49a0b228cee87
LJJ <2366876022@qq.com>
2023-07-04 21:36:07 +0800

    mqtt 1.2

--- mqtt.go
+++ mqtt.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
@@ -16,7 +13,7 @@ func main() {
 	opts.SetClientID("mqtt-client")
 	opts.SetUsername("t1")
 	opts.SetPassword("1234556")
-	// Set the message received handler function
+	// Set the message handler for receiving messages
 	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
 		go func() {
 			fmt.Printf("\nReceived message: %s from topic: %s\n", msg.Payload(), msg.Topic())
@@ -27,22 +24,21 @@ func main() {
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
 	}
-	// Function selection list
+	// Function selection menu
 	fmt.Println("Please select an option:")
 	fmt.Println("1. Add subscription topic")
 	fmt.Println("2. Send message")
-	fmt.Println("3. Quit")
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("3. Exit")
 	for {
+		var option string
 		fmt.Print("Enter your option: ")
-		option, _ := reader.ReadString('\n')
-		option = strings.TrimSuffix(option, "\n")
+		fmt.Scanln(&option)
 		switch option {
 		case "1":
 			// Add subscription topic
-			fmt.Print("Enter the topic to subscribe to: ")
-			topic, _ := reader.ReadString('\n')
-			topic = strings.TrimSuffix(topic, "\n")
+			var topic string
+			fmt.Print("Enter the topic you want to subscribe to: ")
+			fmt.Scanln(&topic)
 			qos := 1
 			if token := client.Subscribe(topic, byte(qos), nil); token.Wait() && token.Error() != nil {
 				log.Fatal(token.Error())
@@ -50,17 +46,16 @@ func main() {
 			fmt.Printf("Subscribed to topic: %s\n", topic)
 		case "2":
 			// Send message
+			var topic, message string
 			fmt.Print("Enter the message topic: ")
-			topic, _ := reader.ReadString('\n')
-			topic = strings.TrimSuffix(topic, "\n")
+			fmt.Scanln(&topic)
 			fmt.Print("Enter the message content: ")
-			message, _ := reader.ReadString('\n')
-			message = strings.TrimSuffix(message, "\n")
+			fmt.Scanln(&message)
 			qos := 1
 			if token := client.Publish(topic, byte(qos), false, message); token.Wait() && token.Error() != nil {
 				log.Fatal(token.Error())
 			}
-			fmt.Printf("Message sent: %s to topic: %s\n", message, topic)
+			fmt.Printf("\nSent message: {\n\"msg\": %s \n} to topic: %s\n", message, topic)
 		case "3":
 			// Exit the program
 			fmt.Println("Program exited")

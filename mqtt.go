e74b351217890d8834481ba11124b813db026baf
LJJ <2366876022@qq.com>
2023-07-04 17:39:18 +0800

    mqtt 1.1

--- mqtt.go
+++ mqtt.go
@@ -1,51 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
-	"time"
+	"os"
+	"strings"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	// 创建一个MQTT客户端实例
+	// Create an MQTT client instance
 	opts := MQTT.NewClientOptions().AddBroker("43.142.90.79:1883")
 	opts.SetClientID("mqtt-client")
 	opts.SetUsername("t1")
 	opts.SetPassword("1234556")
-	// 设置消息接收处理函数
+	// Set the message received handler function
 	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
 		go func() {
-			fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+			fmt.Printf("\nReceived message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 		}()
 	})
-	// 创建一个MQTT客户端
+	// Create an MQTT client
 	client := MQTT.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
 	}
-	// 订阅一个主题
-	topic := "q"
-	qos := 1
-	if token := client.Subscribe(topic, byte(qos), nil); token.Wait() && token.Error() != nil {
-		log.Fatal(token.Error())
+	// Function selection list
+	fmt.Println("Please select an option:")
+	fmt.Println("1. Add subscription topic")
+	fmt.Println("2. Send message")
+	fmt.Println("3. Quit")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter your option: ")
+		option, _ := reader.ReadString('\n')
+		option = strings.TrimSuffix(option, "\n")
+		switch option {
+		case "1":
+			// Add subscription topic
+			fmt.Print("Enter the topic to subscribe to: ")
+			topic, _ := reader.ReadString('\n')
+			topic = strings.TrimSuffix(topic, "\n")
+			qos := 1
+			if token := client.Subscribe(topic, byte(qos), nil); token.Wait() && token.Error() != nil {
+				log.Fatal(token.Error())
+			}
+			fmt.Printf("Subscribed to topic: %s\n", topic)
+		case "2":
+			// Send message
+			fmt.Print("Enter the message topic: ")
+			topic, _ := reader.ReadString('\n')
+			topic = strings.TrimSuffix(topic, "\n")
+			fmt.Print("Enter the message content: ")
+			message, _ := reader.ReadString('\n')
+			message = strings.TrimSuffix(message, "\n")
+			qos := 1
+			if token := client.Publish(topic, byte(qos), false, message); token.Wait() && token.Error() != nil {
+				log.Fatal(token.Error())
+			}
+			fmt.Printf("Message sent: %s to topic: %s\n", message, topic)
+		case "3":
+			// Exit the program
+			fmt.Println("Program exited")
+			client.Disconnect(250)
+			return
+		default:
+			fmt.Println("Invalid option, please try again")
+		}
 	}
-	fmt.Printf("Subscribed to topic: %s\n", topic)
-	// 发布一条消息
-	message := "pcp"
-	if token := client.Publish(topic, byte(qos), false, message); token.Wait() && token.Error() != nil {
-		log.Fatal(token.Error())
-	}
-	fmt.Printf("Published message: %s to topic: %s\n", message, topic)
-	// 等待一段时间，以便接收到消息
-	time.Sleep(60 * time.Second)
-	// 取消订阅主题
-	if token := client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
-		log.Fatal(token.Error())
-	}
-	fmt.Printf("Unsubscribed from topic: %s\n", topic)
-	// 断开MQTT连接
-	client.Disconnect(250)
-	fmt.Println("Disconnected from MQTT broker")
 }

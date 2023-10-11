5f77f4e96a375d58a490a60da880b3c575541ec8
LJJ <2366876022@qq.com>
2023-10-11 12:31:10 +0800

    fix mqttclient

--- go/client.go
+++ go/client.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"time"
+
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -13,6 +15,9 @@ func main() {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker("lostfind.cn:1883") // 设置MQTT代理服务器地址
 	opts.SetClientID("receiveclient")
+	timestamp := time.Now().Unix() // 获取当前时间戳
+	clientID := fmt.Sprintf("receiveclient_%d", timestamp)
+	opts.SetClientID(clientID)
 
 	// 创建MQTT客户端
 	client := mqtt.NewClient(opts)
@@ -23,8 +28,13 @@ func main() {
 	// 接收消息的回调函数
 	messageHandler := func(client mqtt.Client, msg mqtt.Message) {
 		fmt.Printf("Recevie topic[%s]  message: %s\n", msg.Topic(), msg.Payload())
+		if (msg.Topic() == "exit") {
+			fmt.Printf("    remot-eclient log out safely.\n")
+		}
+		if (msg.Topic() == "error") {
+			fmt.Printf("    remot-eclient lost connection, please try again later.\n")
+		}
 	}
-	
 
 	// 订阅主题
 	topics := []string{"lost", "find", "exit", "error"}
